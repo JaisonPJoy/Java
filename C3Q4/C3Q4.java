4c9c4b41b45b44a9f85b6d9df297920b0b43bccd
JaisonPJoy <76118428+JaisonPJoy@users.noreply.github.com>
2021-09-07 09:42:11 +0530

    Add files via upload

---
+++ C3Q4/C3Q4.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.applet.Applet;
+public class C3Q4 extends Applet
+{
+	public void paint(Graphics g)
+	{
+
+		g.drawRect(70,100,30,30);
+		g.drawRect(110,100,30,20);
+		g.drawOval(150,100,30,30);
+		g.drawOval(190,100,30,20);
+	}
+}
+/*
+<applet code = "C3Q4.class" width="400" height="400"></applet>
+*/
